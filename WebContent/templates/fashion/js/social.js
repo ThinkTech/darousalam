b4685290942eeb0b4ee3fa7b2a5af0bd2d2020ce
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-08-25 09:37:19 +0000

    improving the project.

--- WebContent/templates/fashion/js/social.js
+++ WebContent/templates/fashion/js/social.js
@@ -12,6 +12,20 @@ gigya.socialize.showLoginUI({
 	,
 	cid : ''
 });
+gigya.socialize.showLoginUI({
+	height : 100,
+	width : 330,
+	enabledProviders : 'facebook,twitter,googleplus,linkedin,yahoo,instagram',
+	showTermsLink : false // remove 'Terms' link
+	,
+	showWhatsThis : true // Pop-up a hint describing the Login Plugin, when
+							// the user rolls over the Gigya link.
+	,
+	containerID : 'toploginDiv' // The component will embed itself inside the
+								// loginDiv Div
+	,
+	cid : ''
+});
 gigya.socialize.addEventHandlers({
 	onLogin : onLoginHandler,
 	onLogout : onLogoutHandler,
--- WebContent/templates/fashion/js/template.js
+++ WebContent/templates/fashion/js/template.js
@@ -1,4 +1,32 @@
 var user;
+const payment = {};
+const items = [ 'rotateIn', 'flipInX', 'lightSpeedIn', 'rotateIn',
+				'rollIn', 'zoomIn', 'slideInUp', 'bounceInUp', 'pulse',
+				'rubberBand', 'shake', 'headshake', 'jackInTheBox',
+				'flash', 'swing', 'fadeInUpBig', 'rotateInDownLeft',
+				'rotateInDownRight', 'rotateInUpLeft', 'rotateInUpRight',
+				'zoomInDown', 'zoomInLeft', 'zoomInRight', 'zoomInUp',
+				'bounceIn', 'bounceInDown', 'bounceInLeft',
+				'bounceInRight', 'bounceInUp' ];
+
+
+const saveOrder = function() {
+	const wizard = $("#checkout-wizard").fadeOut(100,function(){
+		$("form",wizard).easyWizard('goToStep', 1);
+		$("#cart ul li").remove();
+		$(".article-count").html(0);
+		$(".simpleCart_quantity").html(0);
+        $('body').css("overflow-y","auto");
+	});
+	const form = $("form",wizard);
+	/*app.post(form.attr("action"),form.serialize(),function(response) {
+		$('html,body').animate({scrollTop:0},100,function(){
+			$("#order-confirmation").fadeIn(100).removeAttr('class').addClass("animated zoomInDown");
+		});
+	}, function(error) {
+		alert("error");
+	});*/
+};
 
 jQuery(document).ready(function($) {
 	$(".scroll").click(function(event){		
@@ -58,6 +86,10 @@ $(document).ready(function() {
 
 $(document).ready(function(){
 
+	$(".w3l_logo h1").addClass("animated "+ items[Math.floor(Math.random() * items.length)]);
+	$(".banner a h4").addClass("animated zoomIn");
+	$(".banner h6").addClass("animated zoomIn");
+	
 	/* ---- Countdown timer ---- */
 
 	$('#counterTop').countdown({
@@ -90,23 +122,83 @@ $(document).ready(function(){
 	});
 		
 	$(".checkout").click(function(){
-		const count = $("#cart ul li").length;
+		const cart = $("#cart"); 
+		const count = $("ul li",cart).length;
 		const message = "votre panier est vide";
 		if(!count) {
-			$("#cart").hide();
+			cart.hide();
 			alert(message);
 		}else {
-			/*payment.done = false;
+			payment.done = false;
 		    const wizard = $("#checkout-wizard").css("height",$(document).height());
-		    const top = $("#shopping").offset().top;
+		    const top = cart.offset().top;
 		    $('html,body').animate({scrollTop:top},100,function(){
 		    	$("> div",wizard).css("top",top+20);
 		    	 wizard.show();
 		    });
-		    $('body').css("overflow-y","hidden");*/
+		    $('body').css("overflow-y","hidden");
 		}
 		return false;
 	});
+	
+	$(".wizard-close").click(function(){
+		const wizard = $("#checkout-wizard").fadeOut(100);
+		$('body').css("overflow-y","auto");
+	  });
+	  const form = $(".checkout-wizard-steps > form");
+	  form.easyWizard({
+	    prevButton: "Pr\u0117c\u0117dent",
+	    nextButton: "Suivant",
+	    submitButtonText: "Terminer",
+	    before: function(wizardObj,currentStep,nextStep) {
+	    	//if(!user) {
+	    		//alert("vous devez vous connecter");
+	    		//return false;
+	    	//}
+	    },
+	    after: function(wizardObj,prevStep,currentStep) {
+	    	const div = $(".shopping-payment",currentStep);
+	    	if(div.length) {
+	    		$(".payment",currentStep).hide();
+	    		var input = prevStep.find("input[name='payment']:checked");
+	    		var val = input.val();
+	    		if(val=="online") {
+	    			input = prevStep.find("select[name='method']");
+	    			val = input.val();
+	    			payment.done = false;
+	    		}else {
+	    			payment.done = true;
+	    		}
+	    		$("."+val+"-payment",currentStep).show();
+	    	}
+	    },
+	    beforeSubmit: function(wizardObj) {
+	    	if(payment.done) {
+		    	saveOrder();
+	    	}else {
+	    		const radio = form.find("input[name='payment'][value='online']");
+	    		const checked = radio.is(":checked");
+	    		if(checked) {
+	    		  const select = form.find("select[name='method']")
+	    		  const val = select.val();
+	    		  if(val == "visa" || val == "mastercard" || val == "express" || val == "discover") {
+	    			  alert("vous devez effectuer le paiement de votre commande",function(){
+			    			$(".v-button").click();
+			    	  });  
+	    		  }else {
+	    			  saveOrder();
+	    		  }    		  
+	    		}else {
+	    			saveOrder();
+	    		}
+	    	}
+	    	return false;
+	    }
+	  });
+	  form.find("select[name='method']").click(function(){
+		form.find("input[name='payment'][value='online']").prop("checked",true);
+	  });
+	  $("#checkout-wizard").hide();	
 	
 	$('.shop' ).on( 'click', function() {
 		$(this).closest(".modal-content").find(".close").click();
