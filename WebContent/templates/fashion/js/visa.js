52a75801679e99230c9d91b4a3c8dccaddcacd0a
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-08-25 10:00:01 +0000

    improving the project.

---
+++ WebContent/templates/fashion/js/visa.js
@@ -0,0 +1,34 @@
+function onVisaCheckoutReady(){
+  V.init( {
+  apikey: "5CUQJ9M76DYS2QYARXZZ21PcguqrizMxsdocAavPttpscAbNU",
+  paymentRequest:{
+    currencyCode: "USD",
+    subtotal: "11.00",
+    shippingHandling: "2.00",
+    tax: "0.00",
+    discount: "0.00",
+    giftWrap: "0.00",
+    total : "13.00"
+  },
+  settings: {
+	  locale: "fr_FR",
+	  displayName: "La Boutique Darou Salam",
+	  websiteUrl: "http://darousalam.mircloud.host",
+	  shipping: {
+		  collectShipping : "false"
+	  },
+	  review: {
+		  message: "Effectuer le paiement de votre commande.",
+		  buttonAction : "Pay"
+	  }	
+  } 
+  });
+  V.on("payment.success", function(response){
+	payment.done = true;
+	saveOrder();
+  });
+  V.on("payment.cancel", function(response){ 
+  });
+  V.on("payment.error", function(response, error){ 
+  });
+}
