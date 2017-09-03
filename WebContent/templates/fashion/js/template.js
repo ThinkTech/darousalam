057eab59c4f136d4a071d17e96c6696f730d4d7c
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-09-03 22:16:59 +0000

    improving the project.

--- WebContent/templates/fashion/js/template.js
+++ WebContent/templates/fashion/js/template.js
@@ -367,30 +367,7 @@ $(document).ready(function(){
 		  div = $("#top-brands");
 		  if($(this).scrollTop() >= div.position().top-400) {
 			if(div.is(":visible")) {
-				loadImages(div,function(){
-					$("#flexiselDemo1").flexisel({
-						visibleItems: 4,
-						animationSpeed: 1000,
-						autoPlay: true,
-						autoPlaySpeed: 3000,    		
-						pauseOnHover: true,
-						enableResponsiveBreakpoints: true,
-						responsiveBreakpoints: { 
-							portrait: { 
-								changePoint:480,
-								visibleItems: 1
-							}, 
-							landscape: { 
-								changePoint:640,
-								visibleItems:2
-							},
-							tablet: { 
-								changePoint:768,
-								visibleItems: 3
-							}
-						}
-					});
-				}); 
+				loadImages(div); 
 			}
 		  }
 		  div = $("#footer");
@@ -416,4 +393,29 @@ $(document).ready(function(){
 				}
 		  }
 	});
+	div = $("#top-brands");
+	if(div.is(":visible")){
+		$("#flexiselDemo1").flexisel({
+			visibleItems: 4,
+			animationSpeed: 1000,
+			autoPlay: true,
+			autoPlaySpeed: 3000,    		
+			pauseOnHover: true,
+			enableResponsiveBreakpoints: true,
+			responsiveBreakpoints: { 
+				portrait: { 
+					changePoint:480,
+					visibleItems: 1
+				}, 
+				landscape: { 
+					changePoint:640,
+					visibleItems:2
+				},
+				tablet: { 
+					changePoint:768,
+					visibleItems: 3
+				}
+			}
+		});	
+	}
 });
