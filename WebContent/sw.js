3b8b72a6a2cb09a5fd42eba997998c9d08aa3713
Mamadou Lamine Ba <lamine.ba@thinktech.sn>
2017-09-27 12:12:51 +0000

    improving the project.

--- WebContent/sw.js
+++ WebContent/sw.js
@@ -9,12 +9,13 @@ self.addEventListener('install', function(event) {
   );
 });
 self.addEventListener('fetch', function(event) {
+	  const request = event.request;
 	  event.respondWith(
 	    caches.open('assets').then(function(cache) {
-	      return cache.match(event.request).then(function (response) {
-	        return response || fetch(event.request).then(function(response) {
-	          if(event.request.url.indexOf("/images/")!=-1){
-	        	 cache.put(event.request, response.clone());
+	      return cache.match(request).then(function (response) {
+	        return response || fetch(request).then(function(response) {
+	          if(request.url.indexOf("/images/")!=-1 || request.url.indexOf("gigya.com")!=-1){
+	        	 cache.put(request, response.clone());
 	          }
 	          return response;
 	        });
